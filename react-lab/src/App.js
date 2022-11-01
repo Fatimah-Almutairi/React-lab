b99a8fd984adcc5ffb97fb2943042ee6f8a8dc4c
fatimah06 <fatima.m.mutairi@gmail.com>
2022-11-01 09:57:57 +0300

    search

--- react-lab/src/App.js
+++ react-lab/src/App.js
@@ -9,18 +9,14 @@ import {Array} from '../src/component/Cards';
 // import Search from './component/Search';
 function App() {
         const [search,setSearch] = useState('');
-        const find = Array.filter(item => item.search.toLowerCase().includes(search));
+        const find = Array.filter(item => item.search);
   return (
     < >
     <Nav>
 
-                <Input
-                  type="text"
-                  placeholder="Search..."
-                  onChange={(e) => {
+            <Input type="text" placeholder="Search..." onChange={(e) => {
                     e.preventDefault();
-                    setSearch(e.target.value);
-                  }} />
+                    setSearch(e.target.value); }} />
                       <Button>
                   <Image src="search.png" width="90%" height="80%" />
                 </Button>
@@ -53,6 +49,56 @@ function App() {
             join = {item.join}
             > 
     </Cards>))}
+
+    <Cards Image = {"Js.png"}
+            name =  {"معسكر تطوير تطبيقات الويب باستخدام Javascript"}
+            description = {'يمكنك هذا المعسكر من بناء وتطوير تطبيقات ويب تفاعلية متكاملة باستخدام JavaScript'}
+            details = {'التفاصيل'}
+            join = {false}
+            >
+
+    </Cards>
+
+    <Cards Image = {"figma.png"}
+            name =  {"UI/UX من البداية حتى الاتقان باستخدام Figma"}
+            description = {'دورة UI/UX نبدأ بأساسيات UI حتى مراحل الإتقان باستخدام Figma'}
+            details = {'التفاصيل'}
+            join = {true}
+            >
+
+    </Cards>
+
+    <Cards Image = {"Apple.png"}
+            name =  {"تطوير تطبيقات iOS باستخدام SwiftUI"}
+            description = {'برنامج تدريبي لتطوير تطبيقات iOS باستخدام إطار عمل SwiftUI الذي يعتبر الإطار الأحدث والأسهل لبناء واجهات المستخدم عبر جميع منصات Apple'}
+            details = {'التفاصيل'}
+            join = {false}
+            >
+    </Cards>
+    <Cards Image = {"image1.png"}
+            name =  {"دورة تصميم المواقع بمنهجية No-Code"}
+            description = {'نستهدف في هذه الدورة الراغبين بتعلم تصميم مواقع الويب من الصفر لتطوير مهاراتهم عن طريق مجموعة من التدريبات العملية والممتعة'}
+            details = {'التفاصيل'}
+            join = {true}
+            >
+    </Cards>
+
+    <Cards Image = {"git.png"}
+            name =  {"دورة Git and Github"}
+            description = {'تعلم أساسيات إدارة مشروعك باستخدام Git &amp;amp;amp;amp;amp;amp; Github والذي يستخدم على نطاق واسع من قبل المطورين من جميع أنحاء العالم'}
+            details = {'التفاصيل'}
+            join = {false}
+            >
+    </Cards>
+
+    <Cards Image = {"image1.png"}
+            name =  {"مقدمة في DevOps"}
+            description = {'تعلم أهمية DevOps وأساسياته'}
+            details = {'التفاصيل'}
+            join = {true}
+            >
+
+    </Cards> 
     </Grid>
 
     </div>
@@ -65,44 +111,5 @@ function App() {
 
 export default App;
 
-//     <Cards Image = {"figma.png"}
-//             name =  {"UI/UX من البداية حتى الاتقان باستخدام Figma"}
-//             description = {'دورة UI/UX نبدأ بأساسيات UI حتى مراحل الإتقان باستخدام Figma'}
-//             details = {'التفاصيل'}
-//             join = {true}
-//             >
 
-//     </Cards>
-
-//     <Cards Image = {"Apple.png"}
-//             name =  {"تطوير تطبيقات iOS باستخدام SwiftUI"}
-//             description = {'برنامج تدريبي لتطوير تطبيقات iOS باستخدام إطار عمل SwiftUI الذي يعتبر الإطار الأحدث والأسهل لبناء واجهات المستخدم عبر جميع منصات Apple'}
-//             details = {'التفاصيل'}
-//             join = {false}
-//             >
-//     </Cards>
-//     <Cards Image = {"image1.png"}
-//             name =  {"دورة تصميم المواقع بمنهجية No-Code"}
-//             description = {'نستهدف في هذه الدورة الراغبين بتعلم تصميم مواقع الويب من الصفر لتطوير مهاراتهم عن طريق مجموعة من التدريبات العملية والممتعة'}
-//             details = {'التفاصيل'}
-//             join = {true}
-//             >
-//     </Cards>
-
-//     <Cards Image = {"git.png"}
-//             name =  {"دورة Git and Github"}
-//             description = {'تعلم أساسيات إدارة مشروعك باستخدام Git &amp;amp;amp;amp;amp;amp; Github والذي يستخدم على نطاق واسع من قبل المطورين من جميع أنحاء العالم'}
-//             details = {'التفاصيل'}
-//             join = {false}
-//             >
-//     </Cards>
-
-//     <Cards Image = {"image1.png"}
-//             name =  {"مقدمة في DevOps"}
-//             description = {'تعلم أهمية DevOps وأساسياته'}
-//             details = {'التفاصيل'}
-//             join = {true}
-//             >
-
-//     </Cards> 
 
